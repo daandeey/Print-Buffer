10257de21565aeebf83bfd19df6ac6c5efdb0020
Ivan Fadillah <44187397+ivanfadillah@users.noreply.github.com>
2019-03-14 23:12:49 +0700

    Add files via upload
    add function awan

--- Garis/garis.h
+++ Garis/garis.h
@@ -12,4 +12,8 @@ void printJalan(int, int);
 void printSawahL(int x0, int y0);
 void printSawahR(int x0, int y0);
 void printSawah(int x0_L, int x0_R, int y0);
+void printAwan(int xp, int yp, int p, int q, int r, int g, int b, int t);
+void printSetengahBawahElips(int xp, int yp, int p, int q, int r, int g, int b, int t);
+void printSetengahAtasLingkaran(int xp, int yp, int z, int r, int g, int b, int t);
+
 #endif
